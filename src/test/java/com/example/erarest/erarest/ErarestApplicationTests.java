dce6dd244b334a807bffb5e6003e8796077f6dae
Mahavirbha Chauhan <70435819+mahavirbha@users.noreply.github.com>
2021-08-24 18:54:34 +0530

    Add files via upload

---
+++ src/test/java/com/example/erarest/erarest/ErarestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erarest.erarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
